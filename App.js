c7403ec23f8947849d20fb21841fd7c177358e7d
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2022-10-08 14:22:39 -0300

    questão 3

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [newMovie, setNewMovie] = useState('');
+  const [newMusic, setNewMusic] = useState('');
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -22,6 +27,25 @@ export default function App() {
           <Text style={styles.musics}>Bad Romance - Lady Gaga</Text>
           <Text style={styles.musics}>Monster - Imagine Dragons</Text>
         </View>
+      </View>
+
+      <View style={styles.inputsContainer}>
+        <TextInput 
+          placeholder='Adicione um filme' 
+          style={styles.inputs}
+          onChangeText={setNewMovie}
+          value={newMovie}
+        />
+        <TextInput 
+          placeholder='Adicione uma música' 
+          style={styles.inputs}
+          onChangeText={setNewMusic}
+          value={newMusic}
+        />
+        <Button 
+          onPress={() => alert(`Novo filme: ${newMovie}\nNova música: ${newMusic}`)}
+          title={'ADD'}
+        />
       </View>
     </View>
   );
@@ -48,5 +72,16 @@ const styles = StyleSheet.create({
   musics: {
     color: '#109',
     fontSize: 12,
+  },
+  inputsContainer: {
+    marginTop: 20,
+  }, 
+  inputs: {
+    borderColor: '#000000',
+    borderWidth: 1,
+    marginBottom: 20,
+    width: 200,
+    padding: 5,
+    borderRadius: 5,
   }
 });
