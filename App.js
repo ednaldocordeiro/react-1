b8ff0ad01ee16a1ebe1addd485363212377cc073
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2022-10-08 13:36:18 -0300

    quetão 1

--- App.js
+++ App.js
@@ -4,8 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text>Ednaldo Cordeiro Lins Junior</Text>
+      </View>
+
+      <View>
+        <View>
+          <Text>FILMES</Text>
+          <Text>Vingadores: Ultimato</Text>
+          <Text>Shang-Chi</Text>
+          <Text>Jogos Vorazes</Text>
+        </View>
+        <View>
+          <Text>MÚSICAS</Text>
+          <Text>Hymn for the Weekend - Coldplay</Text>
+          <Text>Bad Romance - Lady Gaga</Text>
+          <Text>Monster - Imagine Dragons</Text>
+        </View>
+      </View>
     </View>
   );
 }
